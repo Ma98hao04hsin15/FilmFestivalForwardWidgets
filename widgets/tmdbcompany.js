aeced4868e807db5877fdab02574f97dc4e9d028
Ma98hao04hsin15 <84980057+Ma98hao04hsin15@users.noreply.github.com>
2026-02-26 22:27:54 +0800

    Add functions for fetching TV and movie data

--- widgets/tmdbcompany.js
+++ widgets/tmdbcompany.js
@@ -142,8 +142,183 @@ async function fetchData(api, params, forceMediaType) {
   }
 }
 
+async function nowPlaying(params) {
+  const type = params.type;
+  let api = "tv/on_the_air";
+  if (type === "movie") {
+    api = "movie/now_playing";
+  }
+  return await fetchData(api, params, type);
+}
+
+async function trending(params) {
+  const timeWindow = params.time_window;
+  const api = `trending/all/${timeWindow}`;
+  delete params.time_window;
+  return await fetchData(api, params);
+}
+
+async function popular(params) {
+  const type = params.type;
+  let api = `movie/popular`;
+  if (type === "tv") {
+    api = `tv/popular`;
+  }
+  delete params.type;
+  return await fetchData(api, params, type);
+}
+
+async function topRated(params) {
+  const type = params.type;
+  let api = `movie/top_rated`;
+  if (type === "tv") {
+    api = `tv/top_rated`;
+  }
+  delete params.type;
+  return await fetchData(api, params, type);
+}
+
+async function categories(params) {
+  let genreId = params.with_genres;
+  let type = params.type;
+  const onlyMovieGenreIds = ["28", "53"];//动作，惊悚
+  const onlyTvGenreIds = ["10762", "10764", "10766"];//儿童，真人秀，肥皂剧
+  if (genreId == "878" && type == "tv") {
+    genreId = "10765";
+  }
+  if (onlyMovieGenreIds.includes(genreId)) {
+    type = "movie";
+  }
+  if (onlyTvGenreIds.includes(genreId)) {
+    type = "tv";
+  }
+  const api = `discover/${type}`;
+  params.with_genres = genreId;
+  delete params.type;
+  return await fetchData(api, params, type);
+}
+
+async function networks(params) {
+  let api = `discover/tv`;
+  delete params.type;
+  return await fetchData(api, params);
+}
+
 async function companies(params) {
   let api = `discover/movie`;
   delete params.type;
   return await fetchData(api, params, "movie");
+}
+
+async function list(params = {}) {
+  let url = params.url;
+
+  // append ?view=grid
+  if (!url.includes("view=grid")) {
+    if (url.includes("?")) {
+      url = url + "&view=grid";
+    } else {
+      url = url + "?view=grid";
+    }
+  }
+
+  console.log("请求片单页面:", url);
+  // 发送请求获取片单页面
+  const response = await Widget.http.get(url, {
+    headers: {
+      Referer: `https://www.themoviedb.org/`,
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    },
+  });
+
+  if (!response || !response.data) {
+    throw new Error("获取片单数据失败");
+  }
+
+
+  console.log("片单页面数据长度:", response.data.length);
+  console.log("开始解析");
+
+  // 解析 HTML 得到文档 ID
+  const $ = Widget.html.load(response.data);
+  if (!$ || $ === null) {
+    throw new Error("解析 HTML 失败");
+  }
+
+  //        // 获取所有视频项，得到元素ID数组
+  const coverElements = $(".block.aspect-poster");
+
+  console.log("items:", coverElements);
+
+  let tmdbIds = [];
+  for (const itemId of coverElements) {
+    const $item = $(itemId);
+    const link = $item.attr("href");
+    if (!link) {
+      continue;
+    }
+    const match = link.match(/^\/(movie|tv)\/([^\/-]+)-/)
+    const type = match?.[1];
+    const id = match?.[2];
+    if (id && type) {
+      tmdbIds.push({ id: `${type}.${id}`, type: 'tmdb' });
+    }
+  }
+
+  return tmdbIds;
+}
+
+function genreTitleWith(genre_ids) {
+  if (!genre_ids) {
+    return "";
+  }
+  const genreDict = [
+    {"id": 10759, "name": "动作冒险"},
+    {"id": 16, "name": "动画"},
+    {"id": 35, "name": "喜剧"},
+    {"id": 80, "name": "犯罪"},
+    {"id": 99, "name": "纪录"},
+    {"id": 18, "name": "剧情"},
+    {"id": 10751, "name": "家庭"},
+    {"id": 10762, "name": "儿童"},
+    {"id": 9648, "name": "悬疑"},
+    {"id": 10763, "name": "新闻"},
+    {"id": 10764, "name": "真人秀"},
+    {"id": 10765, "name": "Sci-Fi & Fantasy"},
+    {"id": 10766, "name": "肥皂剧"},
+    {"id": 10767, "name": "脱口秀"},
+    {"id": 10768, "name": "War & Politics"},
+    {"id": 37, "name": "西部"},
+    {"id": 28, "name": "动作"},
+    {"id": 12, "name": "冒险"},
+    {"id": 16, "name": "动画"},
+    {"id": 35, "name": "喜剧"},
+    {"id": 80, "name": "犯罪"},
+    {"id": 99, "name": "纪录"},
+    {"id": 18, "name": "剧情"},
+    {"id": 10751, "name": "家庭"},
+    {"id": 14, "name": "奇幻"},
+    {"id": 36, "name": "历史"},
+    {"id": 27, "name": "恐怖"},
+    {"id": 10402, "name": "音乐"},
+    {"id": 9648, "name": "悬疑"},
+    {"id": 10749, "name": "爱情"},
+    {"id": 878, "name": "科幻"},
+    {"id": 10770, "name": "电视电影"},
+    {"id": 53, "name": "惊悚"},
+    {"id": 10752, "name": "战争"},
+    {"id": 37, "name": "西部"},
+  ]
+  if (genre_ids.length > 2) {
+    genre_ids = genre_ids.slice(0, 2);
+  }
+  const result = genre_ids.map(id => {
+    const genre = genreDict.find(genre => genre.id == id);
+    if (genre) {
+      return genre.name;
+    }
+    return null;
+  }).filter(genre => genre !== null).join(", ");
+  return result;
 }
